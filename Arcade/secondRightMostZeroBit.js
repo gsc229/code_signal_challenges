cde8d8964e699221805996624bcab0a016e8c1ca
greg <gsc229@gmail.com>
2020-11-11 16:44:36 -0500

    solved swapAdjacentBits in one line

--- Arcade/secondRightMostZeroBit.js
+++ Arcade/secondRightMostZeroBit.js
@@ -1,4 +1,6 @@
 function secondRightmostZeroBit(n) {
+  console.log(~n)
+  console.log(~(n | n + 1) & -~(n | n + 1))
   return 2 ** n.toString(2).split("").reverse().join("").replace("0", "1").indexOf("0")
 }
 
